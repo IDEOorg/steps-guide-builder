ab7b5d560f2faa19b0d61ca4a8d5bca5ba6b8f5c
Matthew Epler <mepler@ideo.org>
2017-11-14 16:34:31 -0500

    adds urlBox ga call

--- src/components/UrlBox/index.js
+++ src/components/UrlBox/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Button';
 import UrlImage from '../UrlImage';
+import GoogleAnalytics from 'react-ga';
+
 import './index.less';
 
 const UrlBox = (props) => {
   return (
     <Button
-      onClick={() => { window.open(props.url); }}
+      onClick={() => { 
+        GoogleAnalytics.event({
+          category: 'ResourcesByText',
+          action: 'click',
+          label: props.text
+        });
+        GoogleAnalytics.event({
+          category: 'ResourcesByURL',
+          action: 'click',
+          label: props.url
+        });
+        window.open(props.url); 
+      }}
       textStyleClass="action_button_text"
       className="action_button"
       url={props.url}
